51f7cd7047e6749a92c6dde0fb8acb9bbbe7d518
serhatbek <serhat.bekk@gmail.com>
2022-08-25 12:29:13 +0300

    app structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
 import './App.css';
+import List from './List';
+import Alert from './Alert';
+import { useState } from 'react';
 
 function App() {
-  return <div className='App'></div>;
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, message: '', type: '' });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Cats love');
+  };
+
+  return (
+    <section className='section-center'>
+      <form onSubmit={handleSubmit} className='grocery-form'>
+        {alert.show && <Alert />}
+        <h3>grocery bud</h3>
+        <div className='form-control'>
+          <input
+            type='text'
+            className='grocery'
+            placeholder='e.g. eggs'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type='submit' className='submit-btn'>
+            {isEditing ? 'edit' : 'submit'}
+          </button>
+        </div>
+      </form>
+      <div className='grocery-container'>
+        <List />
+        <button className='clear-btn'>Clear Items</button>
+      </div>
+    </section>
+  );
 }
 
 export default App;
